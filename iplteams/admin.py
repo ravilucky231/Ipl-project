57c43d775e351673fa63a8623f31f25fb7d3ef92
ravilucky231 <ravikumar23797@gmail.com>
2021-07-12 17:38:35 +0530

    updated files

---
+++ iplteams/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Profile_data
+
+
+
+
+admin.site.register(Profile_data)
+
+# Register your models here.
+
---
+++ iplteams/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class IplteamsConfig(AppConfig):
+    name = 'iplteams'
+
+
+
+
+
